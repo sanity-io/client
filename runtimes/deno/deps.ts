dbd2a3883241a81a2b61c70ba9afda10feaf46a7
Cody Olsen <stipsan@gmail.com>
2022-08-16 03:24:10 +0200

    chore: test deno dts

--- runtimes/deno/deps.ts
+++ runtimes/deno/deps.ts
@@ -1,5 +1,4 @@
 // @TODO replace with a local path once we start exporting ESM
 // import createClient from '../../dist/sanityClient.browser.mjs'
-// @deno-types="../../sanityClient.d.ts"
 import createClient from "https://esm.sh/@sanity/client";
 export { createClient };
