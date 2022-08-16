a72beadcb4ed2abb89f2b6440654064adcad9c64
Cody Olsen <stipsan@gmail.com>
2022-08-16 03:14:15 +0200

    chore: test the client in deno

--- runtimes/deno/client_fetch_test.ts
+++ runtimes/deno/client_fetch_test.ts
@@ -1,13 +1,15 @@
 import { assert } from "https://deno.land/std@0.152.0/testing/asserts.ts";
+import { createClient } from "./deps.ts";
 
 const projectId = "81pocpw8";
 const dataset = "production";
+const apiVersion = "v2021-03-25";
 const query = /* groq */ `count(*[studioVersion == 3])`;
 
 // Ensure the runtime is supposed to be able to run this query correctly
 Deno.test("native fetch", async () => {
   const res = await fetch(
-    `https://${projectId}.apicdn.sanity.io/v1/data/query/${dataset}?query=${
+    `https://${projectId}.apicdn.sanity.io/${apiVersion}/data/query/${dataset}?query=${
       encodeURIComponent(
         query,
       )
@@ -15,4 +17,10 @@ Deno.test("native fetch", async () => {
   );
   const { result: data } = await res.json();
   assert(Number.isInteger(data));
+});
+
+Deno.test("@sanity/client fetch", async () => {
+  const client = createClient({ projectId, dataset, apiVersion, useCdn: true });
+  const data = await client.fetch(query);
+  assert(Number.isInteger(data))
 });
--- runtimes/edge/__tests__/client.fetch.js
+++ runtimes/edge/__tests__/client.fetch.js
@@ -3,13 +3,14 @@ const createClient = require('@sanity/client')
 
 const projectId = '81pocpw8'
 const dataset = 'production'
+const apiVersion = 'v2021-03-25'
 const query = /* groq */ `count(*[studioVersion == 3])`
 
 // Ensure the runtime is supposed to be able to run this query correctly
 test('native fetch', async () => {
   expect.assertions(1)
   const res = await fetch(
-    `https://${projectId}.apicdn.sanity.io/v1/data/query/${dataset}?query=${encodeURIComponent(
+    `https://${projectId}.apicdn.sanity.io/${apiVersion}/data/query/${dataset}?query=${encodeURIComponent(
       query
     )}`
   )
@@ -18,7 +19,7 @@ test('native fetch', async () => {
 })
 test('@sanity/client', async () => {
   expect.assertions(1)
-  const client = createClient({projectId, dataset, useCdn: true})
+  const client = createClient({projectId, dataset, apiVersion, useCdn: true})
   const data = await client.fetch(query)
   expect(data).toEqual(expect.any(Number))
 })
