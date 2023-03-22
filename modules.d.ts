d5e1989c0848873dce5c3fcd31b15743e5b971e3
Cody Olsen <stipsan@gmail.com>
2023-03-22 19:14:47 +0100

    refactor: use built-in typings for `@sanity/eventsource`

--- modules.d.ts
+++ modules.d.ts
@@ -1,9 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '@sanity/eventsource' {
-  // re-exports default window.EventSource
-  export default window.EventSource
-}
-
 declare module 'sse-channel' {
   export default class SseChannel {
     constructor(options?: {jsonEncode?: boolean})
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import GithubActionsReporter from 'vitest-github-actions-reporter'
 import pkg from './package.json'
 
 export const sharedConfig: UserConfig['test'] = {
-  // interopDefault is required for the CJS-only packages we still rely on, like @sanity/eventsource
-  deps: {interopDefault: true},
   // don't use vitest to run Bun and Deno tests
   exclude: [...configDefaults.exclude, 'runtimes/**'],
   // Enable rich PR failed test annotation on the CI
