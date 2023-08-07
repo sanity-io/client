ad951d83835b3a2184e1de082b15876dc950309f
Cody Olsen <stipsan@gmail.com>
2023-08-07 19:31:20 +0200

    fix: restore React Native compatibility
    Fixes #201

--- src/data/encodeQueryString.ts
+++ src/data/encodeQueryString.ts
@@ -12,17 +12,18 @@ export const encodeQueryString = ({
   const searchParams = new URLSearchParams()
   // We generally want tag at the start of the query string
   const {tag, ...opts} = options
-  if (tag) searchParams.set('tag', tag)
-  searchParams.set('query', query)
+  // We're using `append` instead of `set` to support React Native: https://github.com/facebook/react-native/blob/1982c4722fcc51aa87e34cf562672ee4aff540f1/packages/react-native/Libraries/Blob/URL.js#L86-L88
+  if (tag) searchParams.append('tag', tag)
+  searchParams.append('query', query)
 
   // Iterate params, the keys are prefixed with `$` and their values JSON stringified
   for (const [key, value] of Object.entries(params)) {
-    searchParams.set(`$${key}`, JSON.stringify(value))
+    searchParams.append(`$${key}`, JSON.stringify(value))
   }
   // Options are passed as-is
   for (const [key, value] of Object.entries(opts)) {
     // Skip falsy values
-    if (value) searchParams.set(key, `${value}`)
+    if (value) searchParams.append(key, `${value}`)
   }
 
   return `?${searchParams}`
