66c9bec6be366ae38a8746d3841c4ef9d16b793d
Cody Olsen <stipsan@gmail.com>
2023-12-07 14:17:32 +0100

    fix(types): disambiguate `SanityClient` imports

--- src/stega/shared.ts
+++ src/stega/shared.ts
@@ -3,3 +3,15 @@ export * from './SanityStegaClient'
 export {stegaEncodeSourceMap} from './stegaEncodeSourceMap'
 export * from './types'
 export {vercelStegaCleanAll} from './vercelStegaCleanAll'
+
+/**
+ * @deprecated -- Use `import type {SanityClient} from '@sanity/client'` instead
+ * @public
+ */
+export type SanityClient = never
+
+/**
+ * @deprecated -- Use `import type {ObservableSanityClient} from '@sanity/client'` instead
+ * @public
+ */
+export type ObservableSanityClient = never
