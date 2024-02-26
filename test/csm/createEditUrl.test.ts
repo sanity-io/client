48fdcdbcc9838b700d27fbdd3450f2cccadecfc5
rdunk <rupert@rupertdunk.com>
2024-02-26 15:36:56 +0000

    test(csm): add missing `isDraft` param to test cases

--- test/csm/createEditUrl.test.ts
+++ test/csm/createEditUrl.test.ts
@@ -16,25 +16,25 @@ const cases = [
     context: {baseUrl, workspace, tool, id, type},
     path: parseJsonPath("$['foo'][?(@._key=='section-1')][0]['baz'][?(@._key=='section-2')]"),
     expected:
-      'https://test.sanity.studio/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0].baz[_key=="section-2"];tool=content?baseUrl=https://test.sanity.studio&id=homepage&type=page&path=foo[_key=="section-1"][0].baz[_key=="section-2"]&workspace=staging&tool=content',
+      'https://test.sanity.studio/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0].baz[_key=="section-2"];tool=content?baseUrl=https://test.sanity.studio&id=homepage&type=page&path=foo[_key=="section-1"][0].baz[_key=="section-2"]&workspace=staging&tool=content&isDraft=',
   },
   {
     context: {baseUrl: '/', id, type},
     path: parseJsonPath("$['foo'][?(@._key=='section-1')][0]"),
     expected:
-      '/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0]?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]',
+      '/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0]?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]&isDraft=',
   },
   {
     context: {baseUrl: '/', workspace, tool, id, type},
     path: parseJsonPath("$['foo'][?(@._key=='section-1')][0]"),
     expected:
-      '/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0];tool=content?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]&workspace=staging&tool=content',
+      '/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0];tool=content?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]&workspace=staging&tool=content&isDraft=',
   },
   {
     context: {baseUrl: '/', workspace, tool, id, type, projectId, dataset},
     path: parseJsonPath("$['foo'][?(@._key=='section-1')][0]"),
     expected:
-      '/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0];tool=content?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]&workspace=staging&tool=content&projectId=a1b2c3d4&dataset=production',
+      '/staging/intent/edit/mode=presentation;id=homepage;type=page;path=foo[_key=="section-1"][0];tool=content?baseUrl=/&id=homepage&type=page&path=foo[_key=="section-1"][0]&workspace=staging&tool=content&projectId=a1b2c3d4&dataset=production&isDraft=',
   },
 ]
 
