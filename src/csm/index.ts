733183d188a6b450e1512a94d255326fc88a2bd5
Cody Olsen <stipsan@gmail.com>
2025-01-21 19:52:22 +0100

    feat(csm): add document id utils

--- src/csm/index.ts
+++ src/csm/index.ts
@@ -2,7 +2,19 @@ import * as studioPath from './studioPath'
 export {studioPath}
 export * from './applySourceDocuments'
 export {createEditUrl} from './createEditUrl'
-export {getPublishedId} from './getPublishedId'
+export {
+  type DraftId,
+  DRAFTS_FOLDER,
+  getDraftId,
+  getPublishedId,
+  getVersionFromId,
+  getVersionId,
+  isDraftId,
+  isPublishedId,
+  isVersionId,
+  type PublishedId,
+  VERSION_FOLDER,
+} from './draftUtils'
 export {jsonPath, jsonPathToStudioPath, parseJsonPath, studioPathToJsonPath} from './jsonPath'
 export {resolvedKeyedSourcePath} from './resolvedKeyedSourcePath'
 export {resolveEditInfo} from './resolveEditInfo'
