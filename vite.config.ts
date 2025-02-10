470c7ac19812b2a746f17e1e8c6b2e2ef29080b6
Cody Olsen <stipsan@gmail.com>
2025-02-10 16:05:28 +0100

    chore: bump vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 // This is the default config, runs with Node.js globals and doesn't require `npm run build` before executing tests
 
-import {configDefaults, defineConfig, type UserConfig} from 'vitest/config'
-import GithubActionsReporter from 'vitest-github-actions-reporter'
+import {configDefaults, defineConfig, type ViteUserConfig} from 'vitest/config'
 
 import pkg from './package.json'
 
-export const sharedConfig: UserConfig['test'] = {
+export const sharedConfig: ViteUserConfig['test'] = {
   // don't use vitest to run Bun and Deno tests
   exclude: [...configDefaults.exclude, 'runtimes/**', 'test-next/**'],
-  // Enable rich PR failed test annotation on the CI
-  reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
   // Allow switching test runs from using the source TS or compiled ESM
   alias: {
     '@sanity/client/csm': new URL(pkg.exports['./csm'].source, import.meta.url).pathname,
