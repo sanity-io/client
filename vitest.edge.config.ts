9a193d520caf63df5b03afc1ef260110cdb97b0b
Cody Olsen <81981+stipsan@users.noreply.github.com>
2025-02-11 12:16:15 +0100

    chore: actually solve edge runtime flake (#1013)

--- vitest.edge.config.ts
+++ vitest.edge.config.ts
@@ -1,27 +1,29 @@
 // Run tests in the Vercel Edge Runtime, and using its resolution algorithm that
 // chooses worker exports if they exist, if not it tries to look for browser exports.
 
-import {defineConfig} from 'vitest/config'
+import {defineConfig, mergeConfig} from 'vitest/config'
 
 import pkg from './package.json'
-import {sharedConfig} from './vite.config'
+import viteConfig from './vite.config'
 
-export default defineConfig({
-  test: {
-    ...sharedConfig,
-    environment: 'edge-runtime',
-    alias: {
-      '@sanity/client/csm': new URL(pkg.exports['./csm'].source, import.meta.url).pathname,
-      '@sanity/client/stega': new URL(pkg.exports['./stega'].browser.source, import.meta.url)
-        .pathname,
-      '@sanity/client': new URL(pkg.exports['.'].browser.source, import.meta.url).pathname,
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'edge-runtime',
+      alias: {
+        '@sanity/client/csm': new URL(pkg.exports['./csm'].source, import.meta.url).pathname,
+        '@sanity/client/stega': new URL(pkg.exports['./stega'].browser.source, import.meta.url)
+          .pathname,
+        '@sanity/client': new URL(pkg.exports['.'].browser.source, import.meta.url).pathname,
+      },
+      typecheck: {
+        enabled: false,
+      },
     },
-    typecheck: {
-      enabled: false,
+    resolve: {
+      // https://github.com/vercel/next.js/blob/95322649ffb2ad0d6423481faed188dd7b1f7ff2/packages/next/src/build/webpack-config.ts#L1079-L1084
+      conditions: ['edge-light', 'worker', 'browser', 'module', 'import', 'node'],
     },
-  },
-  resolve: {
-    // https://github.com/vercel/next.js/blob/95322649ffb2ad0d6423481faed188dd7b1f7ff2/packages/next/src/build/webpack-config.ts#L1079-L1084
-    conditions: ['edge-light', 'worker', 'browser', 'module', 'import', 'node'],
-  },
-})
+  }),
+)
