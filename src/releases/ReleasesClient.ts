bfce4b0353d851d3fc4549046d8de67c597995ad
Jordan Lawrence <jordanl17@me.com>
2025-03-12 15:01:51 +0000

    feat: adding all release actions

--- src/releases/ReleasesClient.ts
+++ src/releases/ReleasesClient.ts
@@ -219,6 +219,132 @@ export class ObservableReleasesClient {
   ): Observable<RawQueryResponse<SanityDocument[]>> {
     return _getReleaseDocuments(this.#client, this.#httpRequest, releaseId, options)
   }
+
+  /**
+   * Edits an existing release, updating the metadata.
+   *
+   * @param releaseId - The id of the release to edit (with no prefix)
+   * @param patch - The patch operation to apply on the release metadata {@link PatchMutationOperation}
+   */
+  edit(
+    releaseId: string,
+    patch: PatchMutationOperation,
+    options?: BaseActionOptions,
+  ): Observable<SingleActionResult> {
+    const editAction: EditReleaseAction = {
+      actionType: 'sanity.action.release.edit',
+      releaseId,
+      patch,
+    }
+
+    return _action(this.#client, this.#httpRequest, editAction, options)
+  }
+
+  /**
+   * Publishes all documents in a release at once. For larger releases the effect of the publish
+   * will be visible immediately when querying but the removal of the `versions.<releasesId>.*`
+   * documents and creation of the corresponding published documents with the new content may
+   * take some time.
+   *
+   * During this period both the source and target documents are locked and cannot be
+   * modified through any other means.
+   *
+   * @param releaseId - The id of the release to publish (with no prefix)
+   */
+  publish(releaseId: string, options?: BaseActionOptions): Observable<SingleActionResult> {
+    const publishAction: PublishReleaseAction = {
+      actionType: 'sanity.action.release.publish',
+      releaseId,
+    }
+
+    return _action(this.#client, this.#httpRequest, publishAction, options)
+  }
+
+  /**
+   * An archive action is used to remove an active release. The documents that comprise the release
+   * are deleted and therefore no longer queryable.
+   *
+   * While the documents remain in retention the last version can still be accessed using document history endpoint.
+   *
+   * @param releaseId - The id of the release to archive (with no prefix)
+   */
+  archive(releaseId: string, options?: BaseActionOptions): Observable<SingleActionResult> {
+    const archiveAction: ArchiveReleaseAction = {
+      actionType: 'sanity.action.release.archive',
+      releaseId,
+    }
+
+    return _action(this.#client, this.#httpRequest, archiveAction, options)
+  }
+
+  /**
+   * An unarchive action is used to restore an archived release and all documents
+   * with the content they had just prior to archiving.
+   *
+   * @param releaseId - The id of the release to unarchive (with no prefix)
+   */
+  unarchive(releaseId: string, options?: BaseActionOptions): Observable<SingleActionResult> {
+    const unarchiveAction: UnarchiveReleaseAction = {
+      actionType: 'sanity.action.release.unarchive',
+      releaseId,
+    }
+
+    return _action(this.#client, this.#httpRequest, unarchiveAction, options)
+  }
+
+  /**
+   * A schedule action queues a release for publishing at the given future time.
+   * The release is locked such that no documents in the release can be modified and
+   * no documents that it references can be deleted as this would make the publish fail.
+   * At the given time, the same logic as for the publish action is triggered.
+   *
+   * @param releaseId - The id of the release to schedule (with no prefix)
+   */
+  schedule(
+    releaseId: string,
+    publishAt: string,
+    options?: BaseActionOptions,
+  ): Observable<SingleActionResult> {
+    const scheduleAction: ScheduleReleaseAction = {
+      actionType: 'sanity.action.release.schedule',
+      releaseId,
+      publishAt,
+    }
+
+    return _action(this.#client, this.#httpRequest, scheduleAction, options)
+  }
+
+  /**
+   * An unschedule action is used to stop a release from being published.
+   * The documents in the release are considered unlocked and can be edited again.
+   * This may fail if another release is scheduled to be published after this one and
+   * has a reference to a document created by this one.
+   *
+   * @param releaseId - The id of the release to unschedule (with no prefix)
+   */
+  unschedule(releaseId: string, options?: BaseActionOptions): Observable<SingleActionResult> {
+    const unscheduleAction: UnscheduleReleaseAction = {
+      actionType: 'sanity.action.release.unschedule',
+      releaseId,
+    }
+
+    return _action(this.#client, this.#httpRequest, unscheduleAction, options)
+  }
+
+  /**
+   * A delete action is used to delete a published or archived release.
+   * The backing system document will be removed from the dataset.
+   *
+   * @param releaseId - The id of the release to delete (with no prefix)
+   */
+  delete(releaseId: string, options?: BaseActionOptions): Observable<SingleActionResult> {
+    const deleteAction: DeleteReleaseAction = {
+      actionType: 'sanity.action.release.delete',
+      releaseId,
+    }
+
+    return _action(this.#client, this.#httpRequest, deleteAction, options)
+  }
 }
 
 /** @public */
@@ -419,5 +545,131 @@ export class ReleasesClient {
     options?: BaseMutationOptions,
   ): Promise<RawQueryResponse<SanityDocument[]>> {
     return lastValueFrom(_getReleaseDocuments(this.#client, this.#httpRequest, releaseId, options))
+  }
+
+  /**
+   * Edits an existing release, updating the metadata.
+   *
+   * @param releaseId - The id of the release to edit (with no prefix)
+   * @param patch - The patch operation to apply on the release metadata {@link PatchMutationOperation}
+   */
+  edit(
+    releaseId: string,
+    patch: PatchMutationOperation,
+    options?: BaseActionOptions,
+  ): Promise<SingleActionResult> {
+    const editAction: EditReleaseAction = {
+      actionType: 'sanity.action.release.edit',
+      releaseId,
+      patch,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, editAction, options))
+  }
+
+  /**
+   * Publishes all documents in a release at once. For larger releases the effect of the publish
+   * will be visible immediately when querying but the removal of the `versions.<releasesId>.*`
+   * documents and creation of the corresponding published documents with the new content may
+   * take some time.
+   *
+   * During this period both the source and target documents are locked and cannot be
+   * modified through any other means.
+   *
+   * @param releaseId - The id of the release to publish (with no prefix)
+   */
+  publish(releaseId: string, options?: BaseActionOptions): Promise<SingleActionResult> {
+    const publishAction: PublishReleaseAction = {
+      actionType: 'sanity.action.release.publish',
+      releaseId,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, publishAction, options))
+  }
+
+  /**
+   * An archive action is used to remove an active release. The documents that comprise the release
+   * are deleted and therefore no longer queryable.
+   *
+   * While the documents remain in retention the last version can still be accessed using document history endpoint.
+   *
+   * @param releaseId - The id of the release to archive (with no prefix)
+   */
+  archive(releaseId: string, options?: BaseActionOptions): Promise<SingleActionResult> {
+    const archiveAction: ArchiveReleaseAction = {
+      actionType: 'sanity.action.release.archive',
+      releaseId,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, archiveAction, options))
+  }
+
+  /**
+   * An unarchive action is used to restore an archived release and all documents
+   * with the content they had just prior to archiving.
+   *
+   * @param releaseId - The id of the release to unarchive (with no prefix)
+   */
+  unarchive(releaseId: string, options?: BaseActionOptions): Promise<SingleActionResult> {
+    const unarchiveAction: UnarchiveReleaseAction = {
+      actionType: 'sanity.action.release.unarchive',
+      releaseId,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, unarchiveAction, options))
+  }
+
+  /**
+   * A schedule action queues a release for publishing at the given future time.
+   * The release is locked such that no documents in the release can be modified and
+   * no documents that it references can be deleted as this would make the publish fail.
+   * At the given time, the same logic as for the publish action is triggered.
+   *
+   * @param releaseId - The id of the release to schedle (with no prefix)
+   */
+  schedule(
+    releaseId: string,
+    publishAt: string,
+    options?: BaseActionOptions,
+  ): Promise<SingleActionResult> {
+    const scheduleAction: ScheduleReleaseAction = {
+      actionType: 'sanity.action.release.schedule',
+      releaseId,
+      publishAt,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, scheduleAction, options))
+  }
+
+  /**
+   * An unschedule action is used to stop a release from being published.
+   * The documents in the release are considered unlocked and can be edited again.
+   * This may fail if another release is scheduled to be published after this one and
+   * has a reference to a document created by this one.
+   *
+   * @param releaseId - The id of the release to unschedule (with no prefix)
+   */
+  unschedule(releaseId: string, options?: BaseActionOptions): Promise<SingleActionResult> {
+    const unscheduleAction: UnscheduleReleaseAction = {
+      actionType: 'sanity.action.release.unschedule',
+      releaseId,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, unscheduleAction, options))
+  }
+
+  /**
+   * A delete action is used to delete a published or archived release.
+   * The backing system document will be removed from the dataset.
+   *
+   * @param releaseId - The id of the release to delete (with no prefix)
+   */
+  delete(releaseId: string, options?: BaseActionOptions): Promise<SingleActionResult> {
+    const deleteAction: DeleteReleaseAction = {
+      actionType: 'sanity.action.release.delete',
+      releaseId,
+    }
+
+    return lastValueFrom(_action(this.#client, this.#httpRequest, deleteAction, options))
   }
 }
