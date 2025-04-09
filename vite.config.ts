74f483974c2603fbbfb203d986a50a23eab5dc65
Cody Olsen <stipsan@gmail.com>
2025-04-09 11:47:38 +0200

    chore: simplify

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
     coverage: {
       provider: 'v8',
-      reporter: ['html', 'json', 'json-summary'],
+      reporter: ['text', 'html', 'json', 'json-summary'],
       include: ['src/**'],
       reportOnFailure: true,
       clean: true,
