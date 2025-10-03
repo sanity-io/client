5716eb29242372feeac978a5d14bc512d8661f56
Dave Sewell <snocorp@gmail.com>
2025-10-03 08:58:33 -0400

    Fix lint warning

--- src/defineCreateClient.ts
+++ src/defineCreateClient.ts
@@ -49,7 +49,7 @@ export default function defineCreateClientExports<
   const createClient = (config: ClientConfigType) => {
     const clientRequester = defineHttpRequest(envMiddleware, {
       ignoreWarnings: config.ignoreWarnings,
-      lineage: config.lineage
+      lineage: config.lineage,
     })
     return new ClassConstructor(
       (options, requester) =>
