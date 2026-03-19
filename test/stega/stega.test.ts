3dcfca939484ac5af7cd498e75368c460a5d2ffb
Cody Olsen <81981+stipsan@users.noreply.github.com>
2026-03-19 14:03:19 +0100

    fix(stega): allow zero-width space characters in content (#1204)
    * chore: use `@vercel/stega` in test suites
    
    * chore: add repro from #1202
    
    * fix: stop inlining `@vercel/stega`'s vercelStegaCombine method
    
    * fix: bump `@vercel/stega` to pass tests

---
+++ test/stega/stega.test.ts
@@ -0,0 +1,12 @@
+import {vercelStegaCombine, vercelStegaDecode} from '@vercel/stega'
+import {expect, test} from 'vitest'
+
+test('it handles trailing zero-width space characters', () => {
+  const payload = 'foo.\u200b'
+  const editInfo = JSON.stringify({origin: 'sanity.io', href: '/studio'})
+  const encoded = vercelStegaCombine(payload, editInfo)
+  expect(encoded).not.toEqual(payload)
+  const decoded = JSON.parse(vercelStegaDecode(encoded)!) as {origin: string; href: string}
+  expect(decoded.origin).toEqual('sanity.io')
+  expect(decoded.href).toEqual('/studio')
+})
